8f52a153de8d0045ce28f8d36c84ff95e77bfdf6
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-15 02:37:55 +0600

    category table foreign key bug fix & factory added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Str;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+    }
 
     public function parent_category()
     {
