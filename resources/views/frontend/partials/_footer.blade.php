636e8710ee0cea9ae4f0de09f7e722036f24a837
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-15 10:21:43 +0600

    master layout done

---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="text-muted py-5">
+    <div class="container">
+        <p class="float-end mb-1">
+            <a href="#">Back to top</a>
+        </p>
+        <p class="mb-1">&copy; {{ config('app.name') }}  {{ date('Y') }}</p>
+        <p class="mb-0">
+            Read our <a href="#">Privacy policy</a> or <a href="#">Contact us</a>.
+        </p>
+    </div>
+</footer>
