d610de3c91e893d5fe8ad42de0e6ae16a639b09d
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-15 16:12:44 +0600

    added frontend link

--- resources/views/frontend/partials/_header.blade.php
+++ resources/views/frontend/partials/_header.blade.php
@@ -41,7 +41,7 @@
                     <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
                     <circle cx="12" cy="13" r="4" />
                 </svg>
-                <strong>{{ config('app.name') }}</strong>
+                <strong><a href="{{ route('frontend.home') }}">{{ config('app.name') }}</a></strong>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
                 aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
