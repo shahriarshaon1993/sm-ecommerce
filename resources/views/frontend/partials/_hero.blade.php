b9add74c37e1098a27f1f530e2b514647647e72a
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-15 15:26:18 +0600

    home page added & design

---
+++ resources/views/frontend/partials/_hero.blade.php
@@ -0,0 +1,14 @@
+<section class="py-5 text-center container">
+    <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">{{ config('app.name') }}</h1>
+            <p class="lead text-muted">
+                Buy anything from our website.
+            </p>
+            <p>
+                <a href="#" class="btn btn-primary my-2">Main call to action</a>
+                <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+            </p>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.home');
+Route::namespace('Frontend')->group(function () {
+    Route::get('/', [HomeController::class, 'showHomePage']);
 });
